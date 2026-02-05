dfc768ae20e6addd081100e9b620b55be5fa50b7
Rajamouli Khamma <khammarajamouli@gmail.com>
2026-02-05 17:31:38 +0530

    Hello World

--- 09-special-variables.sh
+++ 09-special-variables.sh
@@ -3,3 +3,8 @@
 echo "All the special variables in this script are: $*"
 echo "All the special variables in this script are: $@"
 echo "The name of the script is: $0"
+echo "Current working directory is: $PWD"
+echo "Who executed the script: $USER"
+echo "Home directory of the user: $HOME"
+echo "The process ID of the current script is: $$"
+
