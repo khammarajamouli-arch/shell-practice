8070f4a8629ee11c79c00ebbc318bb727dcdc2f1
Rajamouli Khamma <khammarajamouli@gmail.com>
2026-02-05 15:06:28 +0530

    Hello World

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=John
+PERSON2=Rahimm
+
+echo "$PERSON1:: Hi suresh, How are you?"
+echo "$PERSON2:: Hello $PERSON1, I am fine. How are you?"
+echo "$PERSON1:: What are you learning recently?"
+echo "$PERSON2:: I am learning Shell scripting"
