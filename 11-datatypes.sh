80e41d872b1837fa06405b3e1c70937062ceed02
Rajamouli Khamma <khammarajamouli@gmail.com>
2026-02-05 17:57:54 +0530

    Hello World

--- 11-datatypes.sh
+++ 11-datatypes.sh
@@ -4,3 +4,8 @@
 LEADERS=("Alice" "Bob" "Charlie" "Diana")
 
 echo "Leaders in the team are: ${LEADERS[@]}"
+echo "First leader is: ${LEADERS[0]}"
+echo "Second leader is: ${LEADERS[1]}"
+echo "Third leader is: ${LEADERS[2]}"
+echo "Fourth leader is: ${LEADERS[3]}"
+echo "Total number of leaders: ${#LEADERS[@]}"
