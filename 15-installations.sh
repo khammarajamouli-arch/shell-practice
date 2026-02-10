217688f843f17bc3b98c16ac6f71052073bf01d9
Rajamouli Khamma <khammarajamouli@gmail.com>
2026-02-10 12:41:05 +0530

    Hello World

--- 15-installations.sh
+++ 15-installations.sh
@@ -5,3 +5,12 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]; then
     echo "ERROR:: Please run this script with root privilage"
 fi
+
+
+dnf install mysql -y 
+
+if [ $? -ne 0 ]; then
+    echo "ERROR:: Installing mysql is failure"
+else
+    echo  "Installing mysql is success"
+fi
