94ce80e751a35f1a719d24148c5213ccb7f1c7d8
Rajamouli Khamma <khammarajamouli@gmail.com>
2026-02-10 13:40:12 +0530

    Hello World

---
+++ 19-colours.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+
+USERID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Please run this script with root privilage"
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+         echo -e "Installing $2 ... $R is failure $N"
+        exit 1
+    else
+        echo  -e "Installing $2 .... $G is success $N"
+    fi
+
+}
+
+dnf list installed mysql
+if [$? -ne 0 ]; then
+   dnf install mysql -y 
+   VALIDATE $? "mysql"
+else
+   echo -e "mysql already exists ... $Y skipping $N"
+fi
+
+dnf list installed nginx
+if [$? -ne 0 ]; then
+    dnf install nginx -y
+    VALIDATE $? "nginx"
+else
+    echo "nginx already exists ... $Y skipping $N"
+fi
+
+dnf list installed python3
+if [$? -ne 0 ]; then
+    dnf install python3 -y
+    VALIDATE $? "python"
+else
+    echo "python already exists ... $Y skipping $N"
+fi
