a41f95d2bc89685d84bf5f50aafca78472cbafc0
Rajamouli Khamma <khammarajamouli@gmail.com>
2026-02-13 15:36:05 +0530

    hello world

---
+++ 24-script-1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+NAME=INDIA
+echo "My Country: $NAME"
+echo "PID of SCRIPT-1: $$"
+sh 25-script-2.sh
---
+++ 25-script-2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+
+NAME=INDIA
+echo "My Country: $NAME"
+echo "PID of SCRIPT-2: $$"
